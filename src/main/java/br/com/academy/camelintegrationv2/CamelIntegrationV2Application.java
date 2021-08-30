6f4ab80a292963675a3e478e1c0ed652bf4bd932
Nesomar Ramalho Freire <nfreire@latam.stefanini.com>
2021-08-30 18:05:09 -0300

    estrutura inicial do projeto

---
+++ src/main/java/br/com/academy/camelintegrationv2/CamelIntegrationV2Application.java
@@ -0,0 +1,13 @@
+package br.com.academy.camelintegrationv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelIntegrationV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelIntegrationV2Application.class, args);
+	}
+
+}
